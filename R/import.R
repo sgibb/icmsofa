4546eba601a7c2794b42145157feb01c61ba4b52
Sebastian Gibb <mail@sebastiangibb.de>
2018-03-17 10:57:01 +0100

    Add new importXl and importIcm

--- R/import.R
+++ R/import.R
@@ -1,21 +1,21 @@
 #' Import ICM data
 #'
-#' Import ICM SQL-Export.
+#' Import ICM XLS-Export.
 #'
 #' @param file `character`, filename
 #' @param sheets `character`, sheet names
-#' @param columns `character`, colum names
+#' @param columns `character`, column names
 #' @return data.frame
 #' @export
-importIcm <- function(file, sheets=c(PAO="PO2",
-                                     FIO="FIO",
-                                     NOR="Noradrenalin",
-                                     DOB="Dobutamin",
-                                     IBP="IBPm",
-                                     BIL="Bilirubin",
-                                     PLT="Thrombozyten",
-                                     CRE="Kreatinin"),
-                      columns=c(Id="fallnummer", Date="admindate",
+importXl <- function(file, sheets=c(PAO2="PO2",
+                                    FIO2="FIO",
+                                    NOR="Noradrenalin",
+                                    DOB="Dobutamin",
+                                    IBP="IBPm",
+                                    BILI="Bilirubin",
+                                    PLT="Thrombozyten",
+                                    CREA="Kreatinin"),
+                      columns=c(CaseId="fallnummer", Date="admindate",
                                 Description="treatmentname", Value="num")) {
     tbl <- lapply(names(sheets), function(nms) {
         sheet <- sheets[nms]
@@ -27,12 +27,49 @@ importIcm <- function(file, sheets=c(PAO="PO2",
         as.data.frame(tbl, stringsAsFactors=FALSE)
     })
     tbl <- do.call(rbind, tbl)
-    ## fix dates
+    colnames(tbl) <- names(columns)
+
+    .import(tbl)
+}
+
+#' Import ICM data
+#'
+#' Import ICM SQL-Export.
+#'
+#' @param file `character`, filename
+#' @param columns `character`, column names
+#' @return data.frame
+#' @export
+importIcm <- function(file,
+                      columns=c(CaseId="FALLNUMMER", Date="ADMINDATE",
+                                TreatmentId="TREATMENTID",
+                                Description="TREATMENTNAME",
+                                Value="NUMVALUE", Duration="DURATION")) {
+    tbl <- read.table(
+        file,
+        dec=",", sep="\t", header=TRUE,
+        stringsAsFactors=FALSE
+    )
+    tbl <- tbl[, columns, drop=FALSE]
+    colnames(tbl) <- names(columns)
+
+    tbl$Type <- .treatmentIdType(tbl$TreatmentId)
+
+    .import(tbl)
+}
+
+#' Fix and Reorder ICM import
+#'
+#' @param tbl `data.frame`
+#'
+#' @noRd
+.import <- function(tbl) {
     tbl$Date <- as.POSIXct(tbl$Date, origin="1970-01-01 00:00:00", tz="UTC")
 
-    tbl$Value[tbl$Type == "IBP"] <- .filterIbp(tbl$Value[tbl$Type == "IBP"])
+    isBp <- grepl("^N?IBP$", tbl$Type)
+    tbl$Value[isBp] <- .filterIbp(tbl$Value[isBp])
 
-    tbl[order(tbl$Id, tbl$Date),]
+    tbl[order(tbl$CaseId, tbl$Date),]
 }
 
 #' Import timepoint data.
