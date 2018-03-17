9b37dc2c3e1c2e592a082938d9863e8f1bdb4124
Sebastian Gibb <mail@sebastiangibb.de>
2018-03-17 10:51:46 +0100

    Adapt unit tests to new package name

--- tests/testthat/test_horovitz.R
+++ tests/testthat/test_horovitz.R
@@ -1,14 +1,14 @@
 context("horovitz")
 
 test_that(".horovitz", {
-    expect_error(sofa:::.horovitz(200:300, 1.0))
-    expect_error(sofa:::.horovitz(200, c(0.9, 1.0)))
-    expect_error(sofa:::.horovitz(0.1, 1.0))
-    expect_error(sofa:::.horovitz(610, 1.0))
-    expect_error(sofa:::.horovitz(200, 0.2))
-    expect_error(sofa:::.horovitz(200, 1.1))
-    expect_equal(sofa:::.horovitz(1:4 * 100, c(0.21, 0.4, 0.6, 0.8)),
+    expect_error(icmsofa:::.horovitz(200:300, 1.0))
+    expect_error(icmsofa:::.horovitz(200, c(0.9, 1.0)))
+    expect_error(icmsofa:::.horovitz(0.1, 1.0))
+    expect_error(icmsofa:::.horovitz(610, 1.0))
+    expect_error(icmsofa:::.horovitz(200, 0.2))
+    expect_error(icmsofa:::.horovitz(200, 1.1))
+    expect_equal(icmsofa:::.horovitz(1:4 * 100, c(0.21, 0.4, 0.6, 0.8)),
                  1:4 * 100 / c(0.21, 0.4, 0.6, 0.8))
-    expect_equal(sofa:::.horovitz(c(100, NA, 200), c(0.21, 0.3, NA)),
+    expect_equal(icmsofa:::.horovitz(c(100, NA, 200), c(0.21, 0.3, NA)),
                  c(100 / 0.21, NA, NA))
 })
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,7 +1,7 @@
 context("utils")
 
 test_that(".daily", {
-    expect_equal(sofa:::.daily(as.POSIXct(
+    expect_equal(icmsofa:::.daily(as.POSIXct(
         c("2014-02-26 22:50:10 UTC", "2014-02-27 00:02:00 UTC",
           "2014-02-27 00:02:00 UTC"))),
         as.POSIXct(c("2014-02-26 00:00:00 UTC", "2014-02-27 00:00:00 UTC",
@@ -9,15 +9,15 @@ test_that(".daily", {
 })
 
 test_that(".fillNa", {
-    expect_equal(sofa:::.fillNa(1:10), 1:10)
-    expect_equal(sofa:::.fillNa(c(NA, 1, NA, NA, 2, NA, 1, NA, 3)),
+    expect_equal(icmsofa:::.fillNa(1:10), 1:10)
+    expect_equal(icmsofa:::.fillNa(c(NA, 1, NA, NA, 2, NA, 1, NA, 3)),
                  c(NA, 1, 1, 1, 2, 2, 1, 1, 3))
-    expect_equal(sofa:::.fillNa(c(1, NA, 2, 1, 3, NA)),
+    expect_equal(icmsofa:::.fillNa(c(1, NA, 2, 1, 3, NA)),
                  c(1, 1, 2, 1, 3, 3))
 })
 
 test_that(".hourly", {
-    expect_equal(sofa:::.hourly(as.POSIXct(
+    expect_equal(icmsofa:::.hourly(as.POSIXct(
         c("2014-02-26 22:50:10 UTC", "2014-02-27 00:02:00 UTC",
           "2014-02-27 00:02:00 UTC"))),
         as.POSIXct(c("2014-02-26 22:00:00 UTC", "2014-02-26 23:00:00 UTC",
@@ -25,15 +25,15 @@ test_that(".hourly", {
 })
 
 test_that(".maxNa", {
-    expect_equal(sofa:::.maxNa(1:10), 10)
-    expect_equal(sofa:::.maxNa(c(NA, 1, NA, NA, 2, NA, 1, NA, 3)), 3)
-    expect_equal(sofa:::.maxNa(c(NA, NA, NA)), NA_integer_)
-    expect_equal(sofa:::.maxNa(NULL), NA_integer_)
+    expect_equal(icmsofa:::.maxNa(1:10), 10)
+    expect_equal(icmsofa:::.maxNa(c(NA, 1, NA, NA, 2, NA, 1, NA, 3)), 3)
+    expect_equal(icmsofa:::.maxNa(c(NA, NA, NA)), NA_integer_)
+    expect_equal(icmsofa:::.maxNa(NULL), NA_integer_)
 })
 
 test_that(".sofaTypeId", {
-    expect_error(sofa:::.sofaTypeId(1:10))
-    expect_equal(sofa:::.sofaTypeId(
+    expect_error(icmsofa:::.sofaTypeId(1:10))
+    expect_equal(icmsofa:::.sofaTypeId(
         c("HOR", "DOB", "NOR", "IBP", "BIL", "PLT", "CRE", "FOO")),
         c(1, 2, 2, 2, 3:5, NA))
 })
