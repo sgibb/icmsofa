ec78c42cd2576af54b2fd52d8665284845e1a3d5
Sebastian Gibb <mail@sebastiangibb.de>
2018-09-07 12:14:44 +0200

    Adapt sofaForTimepoints to new data structure

--- R/timepoints.R
+++ R/timepoints.R
@@ -6,15 +6,25 @@
 #' @return `data.frame`
 #' @export
 sofaForTimepoints <- function(icm, tp) {
-    sofa <- matrix(NA_integer_, nrow=nrow(tp), ncol=ncol(tp) - 1L,
-                   dimnames=list(c(), paste0("Sofa", colnames(tp)[-1L])))
+    sofa <- matrix(NA_integer_, nrow=nrow(tp), ncol=(ncol(tp) - 1L) * 6L,
+                   dimnames=list(c(), paste(
+                                        rep(
+                                            c(.sofaItems, "SOFA"),
+                                            ncol(tp) - 1L
+                                        ),
+                                        colnames(tp)[-1L],
+                                        sep="_"
+                                      )
+                   )
+    )
     for (i in seq_len(nrow(tp))) {
         sb <- icm[icm$CaseId == tp[i, 1L],]
         for (j in seq_len(ncol(tp) - 1L)) {
             sel <- sb$Date <= tp[i, j + 1L]
             m <- which.max(sb$Date[sel])
             if (length(m)) {
-                sofa[i, j] <- sb$Sofa[sel][m]
+                sb <- sb[sel, c(.sofaItems, "SOFA"), drop=FALSE]
+                sofa[i, (j - 1L) * 6L + 1L:6L] <- as.integer(sb[m[1L],, drop=FALSE])
             }
         }
     }
