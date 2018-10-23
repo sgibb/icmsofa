6ba331ad5a3282fa1d3e99dfa26d088661631b9f
Sebastian Gibb <mail@sebastiangibb.de>
2018-10-23 21:11:56 +0200

    Add verbose output to sofaForTimepoints

--- R/timepoints.R
+++ R/timepoints.R
@@ -7,27 +7,43 @@
 #' range to 24 h + lag seconds (e.g. laboratory values take some time)
 #' @param lagOnlyLaboratory `logical` add lag seconds only to the laboratory
 #' values?
+#' @param verbose `logical`, verbose output?
 #' @return `data.frame`
 #' @export
-sofaForTimepoints <- function(icm, tp, lag=0L, lagOnlyLaboratory=TRUE) {
-    sofa <- matrix(NA_integer_, nrow=nrow(tp), ncol=(ncol(tp) - 1L) * 6L,
-                   dimnames=list(c(), paste(
-                                        rep(
-                                            c(.sofaItems, "SOFA"),
-                                            ncol(tp) - 1L
-                                        ),
-                                        rep(
-                                            colnames(tp)[-1L],
-                                            each=6L
-                                        ),
-                                        sep="_"
-                                      )
-                   )
+sofaForTimepoints <- function(icm, tp, lag=0L, lagOnlyLaboratory=TRUE,
+                              verbose=interactive()) {
+    stopifnot(
+        is.data.frame(icm),
+        is.data.frame(tp) || is.matrix(tp),
+        is.numeric(lag) || length(lag) != 1L,
+        is.character(type) || length(type) != 1L
     )
-    for (i in seq_len(nrow(tp))) {
-        sb <- icm[icm$CaseId == tp[i, 1L],,drop=FALSE]
+
+    nc1 <- ncol(tp) - 1L
+    nr <- nrow(tp)
+
+    sofa <- matrix(
+        NA_integer_, nrow=nr, ncol=nc1 * 6L,
+        dimnames=list(
+            c(),
+            paste(
+                rep(c(.sofaItems, "SOFA"), nc1),
+                rep(colnames(tp)[-1L], each=6L),
+                sep="_"
+            )
+        )
+    )
+    if (verbose) {
+        pb <- txtProgressBar(0L, nr * nc1, style=3L)
+        on.exit(close(pb))
+    }
+    for (i in seq_len(nr)) {
+        sb <- icm[icm$CaseId == tp[i, 1L],, drop=FALSE]
         if (nrow(sb)) {
-            for (j in seq_len(ncol(tp) - 1L)) {
+            for (j in seq_len(nc1)) {
+                if (verbose) {
+                    setTxtProgressBar(pb, (i - 1L) * nc + j)
+                }
                 sofa[i, (j - 1L) * 6L + 1L:6L] <-
                     .sofaAt(sb,
                             tp[i, j + 1L],
